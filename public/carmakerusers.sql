83db562df34eebeb40f8d85ae4d68a93d5f1c0e1
J1angel <60635557+J1angel@users.noreply.github.com>
2022-11-18 16:44:53 +0800

    Use this to login on the page: username:adminuser password:adminuser

---
+++ public/carmakerusers.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM carmaker.users
+LIMIT 0, 1000
+
+-- Date: 2022-11-18 16:41
+*/
+INSERT INTO `` (`id`,`name`,`username`,`password`,`created_at`,`updated_at`) VALUES (1,'Admin Test','adminuser','$2y$10$lWxNWTFN0xAQFZz1OitzXuf5K3nBiODKgQfZh0SZmH0.9r2M8eK1a','2020-09-30 18:14:45','2020-09-30 18:14:45');
